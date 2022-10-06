a12d875e31f9cb3e4e25c0c97298f1d2bf3f31e3
nikothedude <simon.prouty@gmail.com>
2022-10-06 07:55:44 -0400

    sync so i can work on it later

--- src/data/console/commands/niko_MPC_loadSettings.java
+++ src/data/console/commands/niko_MPC_loadSettings.java
@@ -5,6 +5,8 @@ import data.utilities.niko_MPC_ids;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.lazywizard.console.BaseCommand;
+import org.lazywizard.console.CommonStrings;
+import org.lazywizard.console.Console;
 
 import java.io.IOException;
 
@@ -16,6 +18,7 @@ public class niko_MPC_loadSettings implements BaseCommand {
         } catch (IOException | JSONException | NullPointerException ex) {
             throw new RuntimeException(niko_MPC_ids.niko_MPC_masterConfig + " loading failed during command execution!");
         }
+        Console.showMessage("Success! Settings have been reloaded.");
         return CommandResult.SUCCESS;
     }
 }
