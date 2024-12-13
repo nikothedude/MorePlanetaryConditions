803647fc21abdd8e00a5819814725caf873515ba
Kherae <cwaldridge@live.com>
2024-12-13 05:47:30 -0800

    hopefully reduce impact somewhat

--- src/data/scripts/campaign/MPC_coronaResistScript.kt
+++ src/data/scripts/campaign/MPC_coronaResistScript.kt
@@ -1,6 +1,7 @@
 package data.scripts.campaign
 
 import com.fs.starfarer.api.campaign.*
+import com.fs.starfarer.api.fleet.FleetAPI
 import com.fs.starfarer.api.fleet.FleetMemberAPI
 import com.fs.starfarer.api.impl.campaign.ids.Stats
 import com.fs.starfarer.api.util.Misc
@@ -11,13 +12,17 @@ import data.utilities.niko_MPC_fleetUtils.counterTerrainMovement
 import data.utilities.niko_MPC_ids
 import data.utilities.niko_MPC_ids.CORONA_RESIST_MEMORY_FLAG
 import data.utilities.niko_MPC_industryIds
+import com.fs.starfarer.api.util.IntervalUtil
+//import org.apache.log4j.Logger;
 
-open class MPC_coronaResistScript(
-    val entity: SectorEntityToken,
-): niko_MPC_baseNikoScript() {
+
+
+open class MPC_coronaResistScript(val entity: SectorEntityToken,): niko_MPC_baseNikoScript() {
+    //var logger: Logger = Logger.getLogger("MPC_coronaResistScript")
     var coronaResistance: Float = 0.0f
     open var terrainMovementDivisor: Float = 40f
-    val affecting: MutableSet<FleetMemberAPI> = HashSet()
+    private val affecting: MutableSet<FleetMemberAPI> = HashSet()
+    var interval = IntervalUtil(1f, 1f);
 
     override fun startImpl() {
         entity.addScript(this)
@@ -32,20 +37,66 @@ open class MPC_coronaResistScript(
         return false
     }
 
+    private fun iterateThroughAffected(){
+        //if(logger==null){logger=Logger.getLogger("MPC_coronaResistScript")}
+        val iter = affecting.iterator()
+        val fleetBin: MutableSet<CampaignFleetAPI> = HashSet()
+
+        while (iter.hasNext()){
+            val it = iter.next()
+            var shouldRemove=false
+            if((it.fleetData==null)||(it.fleetData.fleet==null)){
+                shouldRemove=true
+            }
+            else if (!shouldAffectFleet((it.fleetData.fleet))){
+                if (!fleetBin.contains(it.fleetData.fleet)){
+                    fleetBin.add(it.fleetData.fleet)
+                }
+                shouldRemove=true
+            }
+            if(shouldRemove){
+                //logger.info("MPC_coronaResistScript:shouldRemove:"+it.id)
+                unAffectFleetMember(it)
+                iter.remove()
+            }
+        }
+
+        val iter2=fleetBin.iterator()
+        while (iter2.hasNext()) {
+            val it = iter2.next()
+            it.memoryWithoutUpdate?.unset(CORONA_RESIST_MEMORY_FLAG)
+            iter2.remove()
+        }
+    }
+
     override fun advance(amount: Float) {
         val days = Misc.getDays(amount)
+        if (interval==null){interval = IntervalUtil(1f, 1f);}
 
-        unapplyToFleets()
-
+        interval.advance(days)
+        if(interval.intervalElapsed()) {
+            iterateThroughAffected()
+        }
         interateThroughFleets(days)
     }
 
     open fun unapplyToFleets() {
-        for (member in affecting) {
-            member.fleetData?.fleet?.memoryWithoutUpdate?.unset(CORONA_RESIST_MEMORY_FLAG)
-            member.stats.dynamic.getStat(Stats.CORONA_EFFECT_MULT).unmodify("${entity.id}_coronaEffect")
+        val iter = affecting.iterator()
+        val fleetBin: MutableSet<CampaignFleetAPI> = HashSet()
+        while (iter.hasNext()){
+            val it = iter.next()
+            if (((it.fleetData!=null)&&(it.fleetData.fleet != null))&&(!fleetBin.contains(it.fleetData.fleet))) {
+                fleetBin.add(it.fleetData.fleet)
+            }
+            unAffectFleetMember(it)
+            iter.remove()
         }
         affecting.clear()
+        val iter2=fleetBin.iterator()
+        while (iter2.hasNext()) {
+            val it = iter2.next()
+            it.memoryWithoutUpdate?.unset(CORONA_RESIST_MEMORY_FLAG)
+        }
     }
 
     private fun interateThroughFleets(days: Float) {
@@ -55,24 +106,32 @@ open class MPC_coronaResistScript(
         }
     }
 
+    private fun affectFleetMember(member: FleetMemberAPI){
+        member.stats.dynamic.getStat(Stats.CORONA_EFFECT_MULT).modifyMult(
+            "${entity.id}_MPCspecialCoronaResistance",
+            coronaResistance,
+            "Unknown corona resistance"
+        )
+    }
+
+    private fun unAffectFleetMember(member: FleetMemberAPI){
+        member.stats.dynamic.getStat(Stats.CORONA_EFFECT_MULT).unmodify("${entity.id}_MPCspecialCoronaResistance")
+    }
+
     private fun affectFleet(fleet: CampaignFleetAPI, days: Float) {
         fleet.counterTerrainMovement(days, terrainMovementDivisor) // this doesnt seem to work very well either, its inconsistant between fleets
-
         fleet.memoryWithoutUpdate.set(CORONA_RESIST_MEMORY_FLAG, coronaResistance, 1f)
+
         for (fleetMember in fleet.fleetData.membersListCopy) {
-            fleetMember.stats.dynamic.getStat(Stats.CORONA_EFFECT_MULT).modifyMult(
-                "${entity.id}_MPCspecialCoronaResistance",
-                coronaResistance,
-                "Unknown corona resistance"
-            )
-            affecting += fleetMember
+            if (!affecting.contains(fleetMember)){
+                affectFleetMember(fleetMember)
+                affecting.add(fleetMember)
+            }
         }
     }
 
     open fun shouldAffectFleet(fleet: CampaignFleetAPI): Boolean {
-        if (fleet.isInHyperspaceTransition) return false
-
-        return true
+        return !fleet.isInHyperspaceTransition
     }
 
     open fun getTargetFleets(): MutableSet<CampaignFleetAPI> {
